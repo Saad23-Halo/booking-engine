5405269a6bd881c2a1a769abc862e7d4eab52bbe
haloflights <haloflights38@gmail.com>
2024-01-17 00:57:29 +0530

    added auth & auth test & initial layout

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+
+    <div className="bg-pink-800 py-10">
+        <div className="container mx-auto flex justify-between">
+            <span className="text-3xl text-white font-bold tracking-tight">
+                Haloholidays.com
+            </span>
+            <span className="text-white font-bold tracking-tight flex gap-4">
+                <p className="cursor-pointer">Privacy Policiy</p>
+                <p className="cursor-pointer">Terms of Service</p>
+            </span>
+        </div>
+    </div>
+   );
+};
+
+export default Footer;
